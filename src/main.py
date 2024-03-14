d1e7101c796f85a818e49e4e897da9dd630f7e05
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-03-14 14:29:29 +0100

    Datos nuevos con faker para la lista de nombres.

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
 from flask import Flask, render_template
+from faker import Faker
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+fake= Faker()
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
 
+    cursos = [fake.first_name() for _ in range(10)]
+
+    data = {
+        "titulo": "Index",
+        "mensaje": "Saludos",
+        "cursos": cursos
+    }
+
+    return render_template('index.html', data=data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True, port=8080)
