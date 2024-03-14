3f7ffd298055ef241471b6013267e576970f82d7
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-03-14 13:40:59 +0100

    Adicionamos modo debug, cambiamos puerto a 8080 y creamos ruta index

--- src/main.py
+++ src/main.py
@@ -3,6 +3,11 @@ from flask import Flask
 app=Flask(__name__)
 
 
+@app.route('/')
+def index():
+    return '<h1>Hola Mundo</h1><p>asi mismo loco</p>'
+
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True,port=8080)
