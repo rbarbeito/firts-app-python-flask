611a862f3c701a35f7212ed89b886122299397d7
Rodolfo Barbeito <rbarbeito@gmail.com>
2024-05-29 11:19:05 +0200

    add 404.html file and middlewares

--- src/main.py
+++ src/main.py
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request
-=======
-from flask import Flask, render_template
->>>>>>> 634a536af60625eba1647cc19d90bd571436c3d3
+from flask import Flask, render_template, request, redirect, url_for
 from faker import Faker
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 fake = Faker()
-=======
-fake= Faker()
->>>>>>> 634a536af60625eba1647cc19d90bd571436c3d3
 
 
 @app.route('/')
@@ -19,10 +11,6 @@ def index():
 
     cursos = [fake.first_name() for _ in range(10)]
     # cursos =[]
-<<<<<<< HEAD
-=======
-    
->>>>>>> 634a536af60625eba1647cc19d90bd571436c3d3
 
     data = {
         "titulo": "Mis Cursos",
@@ -34,7 +22,6 @@ def index():
     return render_template('index.html', data=data)
 
 
-<<<<<<< HEAD
 @app.route('/contacto/<nombre>/<edad>')
 def contacto(nombre, edad):
 
@@ -45,6 +32,7 @@ def contacto(nombre, edad):
     }
 
     return render_template('contacto.html', data=data)
+
 
 def query_string():
     print(request)
@@ -57,11 +45,31 @@ def query_string():
     return "ok"
 
 
+def page_not_found(error):
+    # return render_template('404.html'), 404
+    return redirect(url_for('index')) # haciendo redirect
+
+
+
+# contruyendo un middleware
+@app.before_request
+def before_request():
+    print("Ejecutando antes de la request")
+
+
+@app.after_request
+def after_request(response):
+    print("Ejecutando despues de la peticion")
+
+    return response
+
+
+
+
 if __name__ == '__main__':
 
     app.add_url_rule('/query_string', view_func=query_string)
 
-=======
-if __name__ == '__main__':
->>>>>>> 634a536af60625eba1647cc19d90bd571436c3d3
+    app.register_error_handler(404, page_not_found)
+
     app.run(debug=True, port=8080)
